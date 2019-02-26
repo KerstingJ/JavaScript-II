09c495530b214f56d398abeefee102bbfe1c3d68
KerstingJ <Kersting.Josh@Gmail.com>
2019-02-26 17:15:23 -0500

    updated callbacks stretch

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -57,5 +57,5 @@ function removeDuplicates(array, cb) {
   // removeDuplicates removes all duplicate values from the given array.
   // Pass the duplicate free array to the callback function.
   // Do not mutate the original array.
-  return cb(new Set(array)); // is this cheating??
+  return cb(Array.from(new Set(array))); // is this cheating??
 }
