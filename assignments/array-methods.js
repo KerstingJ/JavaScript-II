f9d88b5df3b41025c97f7ada85a10e2db48087ff
KerstingJ <Kersting.Josh@Gmail.com>
2019-02-26 16:40:14 -0500

    finished array-methods.js

--- assignments/array-methods.js
+++ assignments/array-methods.js
@@ -56,28 +56,56 @@ const runners = [{"id":1,"first_name":"Charmain","last_name":"Seiler","email":"c
 // ==== Challenge 1: Use .forEach() ====
 // The event director needs both the first and last names of each runner for their running bibs.  Combine both the first and last names into a new array called fullName. 
 let fullName = [];
+runners.forEach(runner => fullName.push([runner.first_name, runner.last_name]));
 console.log(fullName);
 
 // ==== Challenge 2: Use .map() ====
 // The event director needs to have all the runner's first names converted to uppercase because the director BECAME DRUNK WITH POWER. Convert each first name into all caps and log the result
-let allCaps = [];
+let allCaps = runners.map(runner => runner.first_name.toUpperCase());
 console.log(allCaps); 
 
 // ==== Challenge 3: Use .filter() ====
 // The large shirts won't be available for the event due to an ordering issue.  Get a list of runners with large sized shirts so they can choose a different size. Return an array named largeShirts that contains information about the runners that have a shirt size of L and log the result
-let largeShirts = [];
+let largeShirts = runners.filter(runner => runner.shirt_size === "L");
 console.log(largeShirts);
 
 // ==== Challenge 4: Use .reduce() ====
 // The donations need to be tallied up and reported for tax purposes. Add up all the donations into a ticketPriceTotal array and log the result
-let ticketPriceTotal = [];
+let ticketPriceTotal = runners.reduce((total, current) => total += current.donation, 0);
 console.log(ticketPriceTotal);
 
 // ==== Challenge 5: Be Creative ====
 // Now that you have used .forEach(), .map(), .filter(), and .reduce().  I want you to think of potential problems you could solve given the data set and the 5k fun run theme.  Try to create and then solve 3 unique problems using one or many of the array methods listed above.
 
 // Problem 1
+    // Yodo is a sponser of the fun run and the runners who work there get a fancy shirt we need a list of those runners to give them the special shirt
+const yodoRunners = runners.filter(runner => runner.company_name === "Yodo")
 
 // Problem 2
+    // EMS is on scene for the fun run but we need to be sure we have enough people
+    // big people are more likely to have heart attacks so lets se how many big people we got to make sure we got enough emergency personel
+const bigShirts = runners.filter(runner => runner.shirt_size.includes("X")).map(runner => `${runner.first_name} ${runner.last_name}`);
+console.log(bigShirts);
 
 // Problem 3
+    // there was a competition to see what company brought the most employees so lets see which employer brought the most people
+const countEmployer = {};
+
+runners.forEach(runner => {
+
+    let emps = Object.keys(countEmployer);
+
+    if (!emps.includes(runner.company_name)) { 
+        countEmployer[runner.company_name] = 0;
+    }
+
+    countEmployer[runner.company_name] += 1;
+});
+
+let empCountArray = Object.entries(countEmployer);
+
+empCountArray.sort((item1, item2) => {
+    return item1[1] > item2[1] ? -1 : 1;
+});
+
+console.log(empCountArray[0]);
